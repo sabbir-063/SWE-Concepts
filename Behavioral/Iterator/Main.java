c9da7b02643b5fa75a35199ba84782d8e4b4bb4b
sabbir-063 <202114063@student.mist.ac.bd>
2026-07-16 08:20:43 +0600

    iterator, mediator added

---
+++ Behavioral/Iterator/Main.java
@@ -0,0 +1,98 @@
+package Behavioral.Iterator;
+
+// ==========================================
+// Component 1: Iterator Interface (কমন রুলস)
+// ==========================================
+interface MyIterator {
+    boolean hasNext(); // পরের কোনো আইটেম বাকি আছে কি না?
+    String next();     // পরের আইটেমটা দাও
+}
+
+// ==========================================
+// Component 3: IterableCollection Interface
+// ==========================================
+interface SongCollection {
+    MyIterator createIterator(); // কালেকশনকে অবশ্যই একটি ইটারেটর দিতে হবে
+}
+
+
+// ==========================================
+// Component 4: Concrete Collection (আসল ডাটা হোল্ডার)
+// ==========================================
+class Playlist implements SongCollection {
+    private String[] songs;
+    private int count = 0;
+
+    public Playlist(int size) {
+        songs = new String[size];
+    }
+
+    public void addSong(String songName) {
+        if (count < songs.length) {
+            songs[count] = songName;
+            count++;
+        }
+    }
+
+    // কালেকশনটি তার নিজের জন্য একটি স্পেশাল ইটারেটর বানিয়ে রিটার্ন করবে
+    @Override
+    public MyIterator createIterator() {
+        return new PlaylistIterator(this.songs);
+    }
+}
+
+
+// ==========================================
+// Component 2: Concrete Iterator (আসল লুপ চালানোর লজিক)
+// ==========================================
+class PlaylistIterator implements MyIterator {
+    private String[] songs;
+    private int currentPosition = 0; // কারেন্ট পজিশন ট্র্যাক রাখার জন্য পয়েন্টার
+
+    public PlaylistIterator(String[] songs) {
+        this.songs = songs;
+    }
+
+    @Override
+    public boolean hasNext() {
+        // যদি পয়েন্টারটি অ্যারের লেংথ বা নাল (null) ভ্যালুর চেয়ে ছোট থাকে
+        return currentPosition < songs.length && songs[currentPosition] != null;
+    }
+
+    @Override
+    public String next() {
+        // বর্তমান পজিশনের গানটি রিটার্ন করে পয়েন্টার এক ধাপ বাড়িয়ে দিলো
+        String song = songs[currentPosition];
+        currentPosition++;
+        return song;
+    }
+}
+
+
+// ==========================================
+// Client Code (Main Method)
+// ==========================================
+public class Main {
+    public static void main(String[] args) {
+        
+        // ১. একটি প্লে-লিস্ট বানালাম এবং গান রাখলাম
+        Playlist myPlaylist = new Playlist(3);
+        myPlaylist.addSong("Song 1: Believer");
+        myPlaylist.addSong("Song 2: Shape of You");
+        myPlaylist.addSong("Song 3: Faded");
+
+        // ২. ক্লায়েন্ট প্লে-লিস্টের ইন্টারনাল স্ট্রাকচার (Array) জানে না। 
+        // সে শুধু ইটারেটর চাইলো।
+        MyIterator iterator = myPlaylist.createIterator();
+
+        // ৩. ইউনিভার্সাল স্টাইলে লুপ চালানো
+        System.out.println("--- Playing My Playlist ---");
+        
+        while (iterator.hasNext()) {
+            String song = iterator.next();
+            System.out.println("Playing 🎵: " + song);
+        }
+        
+        System.out.println("--- Playlist Ended ---");
+    }
+}
---
+++ Behavioral/Mediator/Main.java
@@ -0,0 +1,106 @@
+package Behavioral.Mediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// ==========================================
+// Component 1: Mediator Interface
+// ==========================================
+interface ChatMediator {
+    void sendMessage(String message, User user); // ইউজার মেসেজ পাঠালে মিডিয়েটর তা রিসিভ করবে
+    void addUser(User user);                     // চ্যাট রুমে ইউজার অ্যাড করার জন্য
+}
+
+// ==========================================
+// Component 2: Concrete Mediator (আসল চ্যাট রুম)
+// ==========================================
+class ChatRoomImpl implements ChatMediator {
+    // মিডিয়েটর সবার খবরাখবর রাখে
+    private List<User> users;
+
+    public ChatRoomImpl() {
+        this.users = new ArrayList<>();
+    }
+
+    @Override
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    @Override
+    public void sendMessage(String message, User sender) {
+        // যে মেসেজ পাঠিয়েছে তাকে ছাড়া বাকি সবাইকে মেসেজটি পৌঁছে দিবে মিডিয়েটর
+        for (User u : users) {
+            if (u != sender) {
+                u.receiveMessage(message);
+            }
+        }
+    }
+}
+
+
+// ==========================================
+// Component 3: Colleagues (যাদের মধ্যে যোগাযোগ হবে)
+// ==========================================
+abstract class User {
+    protected ChatMediator mediator; // ইউজারের কাছে শুধু মিডিয়েটরের রেফারেন্স থাকবে
+    protected String name;
+
+    public User(ChatMediator mediator, String name) {
+        this.mediator = mediator;
+        this.name = name;
+    }
+
+    public abstract void send(String message);
+    public abstract void receiveMessage(String message);
+}
+
+class ChatUser extends User {
+    public ChatUser(ChatMediator mediator, String name) {
+        super(mediator, name);
+    }
+
+    @Override
+    public void send(String message) {
+        System.out.println(this.name + " Sending Message: " + message);
+        // ইউজার সরাসরি কাউকে না পাঠিয়ে মিডিয়েটরকে মেসেজটি দিয়ে দিলো!
+        mediator.sendMessage(message, this);
+    }
+
+    @Override
+    public void receiveMessage(String message) {
+        System.out.println(this.name + " Received: " + message);
+    }
+}
+
+
+// ==========================================
+// Client Code (Main Method)
+// ==========================================
+public class Main {
+    public static void main(String[] args) {
+        
+        // ১. একটি মিডিয়েটর (চ্যাট রুম) বানালাম
+        ChatMediator chatRoom = new ChatRoomImpl();
+
+        // ২. ইউজারদের তৈরি করলাম এবং তাদের হাতে মিডিয়েটর ধরিয়ে দিলাম
+        User user1 = new ChatUser(chatRoom, "Sabbir");
+        User user2 = new ChatUser(chatRoom, "Musfique");
+        User user3 = new ChatUser(chatRoom, "John");
+
+        // ৩. ইউজারদের চ্যাট রুমে যুক্ত করলাম
+        chatRoom.addUser(user1);
+        chatRoom.addUser(user2);
+        chatRoom.addUser(user3);
+
+        // ৪. ম্যাজিক! সাব্বির মেসেজ পাঠালো, কিন্তু সে জানে না জন বা মুশফিক কে!
+        // সে শুধু চ্যাট রুমকে মেসেজ দিলো। 
+        user1.send("Hi Everyone! How are you?");
+        
+        /* আউটপুট:
+           Sabbir Sending Message: Hi Everyone! How are you?
+           Musfique Received: Hi Everyone! How are you?
+           John Received: Hi Everyone! How are you?
+        */
+    }
+}
